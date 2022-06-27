c0f997a05b70ff6c727e2c372b5a0aa761752f50
Rifjan Jundila <rjndla@gmail.com>
2022-06-27 21:45:02 +0700

    feat: migration
    field destinasi wisata

---
+++ database/migrations/2022_06_27_143418_create_event_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('kategori_event_id')->nullable()->constrained('kategori_event');
+            $table->string('title');
+            $table->string('slug');
+            // $table->('deskripsi');
+            $table->
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event');
+    }
+}
